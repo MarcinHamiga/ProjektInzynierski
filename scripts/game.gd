737926e70a1652c348a2a84d1b943c0ae2799ed5
marcinhamiga <marcin.hamiga@proton.me>
2024-10-23 21:08:29 +0200

    Dodano podwaliny pod menedżer gry

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,11 +1,24 @@
 extends Node
 
+var state_manager: Node
+var scene_manager: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	self.state_manager = $StateManager
+	self.scene_manager = $SceneManager
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var state = self.state_manager.get_state()
+	if state == self.state_manager.GameState.MAIN_MENU:
+		print(delta)
+		pass
+	elif state == self.state_manager.GameState.GAME:
+		print(delta)
+		pass
+	else:
+		print(delta)
+		pass
 	pass

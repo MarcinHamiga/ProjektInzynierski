9159832bf0085bbbf3582bc1a5d76b26294f3d88
marcinhamiga <marcin.hamiga@proton.me>
2024-10-29 14:34:08 +0100

    Zakończono pracę nad podstawowym kontrolerem gry

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,12 +2,20 @@ extends Node
 
 var state_manager: Node
 var scene_manager: Node
+var day_timer: Timer
+var strike_time: Timer
+var strikes: int
+var day: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.state_manager = $StateManager
 	self.scene_manager = $SceneManager
-
+	self.day_timer = $DayTimer
+	self.day_timer.wait_time = 120
+	self.strike_time = $StrikeTimer
+	self.strikes = 0
+	self.day = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,3 +30,9 @@ func _process(delta: float) -> void:
 		print(delta)
 		pass
 	pass
+
+
+func new_game():
+	self.strikes = 0
+	self.day = 1
+	self.day_timer.start()

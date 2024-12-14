510180ef984d3f6d99564be989338a9d17cf051e
Kacper Słupski <kacperslupski02@gmail.com>
2024-12-14 17:36:53 +0100

    Działający dzienniczek, zakładki i strony w zakładkach zczytują dynamicznie dane z jsona i wyświetlają je na ekranie, można przechodzić między zakładkami i stronami. Na razie json zawiera nazwy testowe

---
+++ scripts/journal/journal.gd
@@ -0,0 +1,33 @@
+extends Control
+
+# Zmienna przechowująca dane JSON
+var journal_data = {}
+var pagers = []
+
+@onready var tab_container = $VBoxContainer/TabContainer
+var current_page_index = 1
+
+func _ready():
+	journal_data = Globals.load_json("res://Dane/journal.json")
+	add_tabs()
+	tab_container.connect("tab_changed", Callable(self, "_on_tab_container_tab_changed"))
+
+func add_tabs():
+	var Pager = preload("res://scenes/journal/pager.tscn")
+	
+	for chapter in journal_data["chapters"]:
+		var pager = Pager.instantiate()
+		pager._ready()
+		pager.pager_name = chapter["title"]
+		pager.add_pages()
+		pager.name = chapter["title"]
+		tab_container.add_child(pager)
+		pagers.append(pager)
+		pager.display()
+		
+func _on_tab_container_tab_changed(tab: int) -> void:
+	var chapter_name = tab_container.get_tab_title(tab)
+	for pager in pagers:
+		if pager.name == chapter_name:
+			pager.display()
+			return
---
+++ scripts/journal/pager.gd
@@ -0,0 +1,40 @@
+extends Control
+
+# Zmienna przechowująca treści stron
+var pager_name = ""
+var pages_content = []
+var current_page_index = 0
+var journal_data = {}
+# Odnośniki do elementów interfejsu
+@onready var text_edit = $VBoxContainer/TextEdit
+@onready var prev_button = $VBoxContainer/HBoxContainer/PrevButton
+@onready var next_button = $VBoxContainer/HBoxContainer/NextButton
+
+func _ready():
+	journal_data = Globals.load_json("res://Dane/journal.json")
+
+func add_pages():
+	for chapter in journal_data["chapters"]:
+		if chapter["title"] == pager_name:
+			for i in range(chapter["pages"].size()):
+				pages_content.append(chapter["pages"][i]["content"])
+			text_edit.text = pages_content[current_page_index]
+
+func display():
+	text_edit.text = pages_content[current_page_index]
+
+func _on_prev_button_pressed() -> void:
+	if pages_content.size() == 0:
+		return
+	current_page_index -= 1
+	if current_page_index < 0:
+		current_page_index = pages_content.size() - 1
+	display()
+
+func _on_next_button_pressed() -> void:
+	if pages_content.size() == 0:
+		return
+	current_page_index += 1
+	if current_page_index >= pages_content.size():
+		current_page_index = 0
+	display()
